10b894e1e7eeeb67947b04270e2cbcdb51186b01
Mirror83 <glenochieng045@gmail.com>
2024-07-14 21:15:56 +0300

    Created bare-bones mode selection page

---
+++ lib/mode_selection.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/game.dart';
+
+class ModeSelection extends StatelessWidget {
+  const ModeSelection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const TicTacToeGame(
+                          gameMode: GameMode.playerVsComputer,
+                        )));
+              },
+              child: const Text("You vs Computer")),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const TicTacToeGame(
+                          gameMode: GameMode.playerVsPlayer,
+                        )));
+              },
+              child: const Text("You vs Human")),
+        ],
+      ),
+    );
+  }
+}
