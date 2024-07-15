336d2a8b49bc058b786176f840adf7a39c12ee96
Mirror83 <glenochieng045@gmail.com>
2024-07-15 15:05:59 +0300

    Added custom ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,37 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  ThemeData _themeData() {
+    final themeData = ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xFF2FC5BD),
+          secondary: Color(0xFFF2B237),
+          tertiary: Color(0xFFA9BEC9),
+          error: Colors.red,
+          surface: Color(0xFF192A32),
+          surfaceContainer: Color(0xFF1E3640),
+          onPrimary: Color(0xFF0D4E50),
+          onSecondary: Color(0xFF573B00),
+          onTertiary: Color(0xFF263B44),
+          onError: Colors.black,
+          onSurface: Color(0xFFA9BEC9),
+        ));
+
+    return themeData.copyWith(
+      textTheme: themeData.textTheme.copyWith(
+        bodySmall: themeData.textTheme.bodySmall!.copyWith(
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF192A32),
-          primary: const Color(0xFF2FC5BD),
-          secondary: const Color(0xFFF2B237),
-        ),
-      ),
+      theme: _themeData(),
       home: const ModeSelection(),
     );
   }
