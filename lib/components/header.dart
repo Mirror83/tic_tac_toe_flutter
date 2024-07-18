925a17907ab8577f1628d9bf2a4f53a26b4551fd
Mirror83 <glenochieng045@gmail.com>
2024-07-18 08:44:37 +0300

    Created TurnIndicator

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/board.dart';
 import 'package:tic_tac_toe/components/pseudo_logo.dart';
 
 class Header extends StatelessWidget {
-  final String gameInfo;
+  final BoardToken currentPlayer;
   final void Function() refreshBoardCallBack;
   const Header(
-      {super.key, required this.gameInfo, required this.refreshBoardCallBack});
+      {super.key,
+      required this.currentPlayer,
+      required this.refreshBoardCallBack});
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +18,63 @@ class Header extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           const PseudoLogo(),
-          Text(gameInfo),
+          TurnIndicator(currentPlayer: currentPlayer),
           RefreshButton(refreshBoardCallBack: refreshBoardCallBack),
+        ],
+      ),
+    );
+  }
+}
+
+class TurnIndicator extends StatelessWidget {
+  const TurnIndicator({
+    super.key,
+    required this.currentPlayer,
+  });
+
+  final BoardToken currentPlayer;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      width: 100,
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surfaceContainer,
+        border: BorderDirectional(
+          bottom: BorderSide(
+            color: theme.colorScheme.shadow,
+            width: 4,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Text(
+                currentPlayer == BoardToken.x
+                    ? "X"
+                    : currentPlayer == BoardToken.o
+                        ? "O"
+                        : "-",
+                style: theme.textTheme.titleLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                "TURN",
+                style: theme.textTheme.bodyMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
