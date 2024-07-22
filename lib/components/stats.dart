efa6b1e3a3c486571f3fbabf861f4cf4096d3ee8
Mirror83 <glenochieng045@gmail.com>
2024-07-22 11:32:41 +0300

    Extracted stats widgets into their own file

---
+++ lib/components/stats.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class StatBlock extends StatelessWidget {
+  final Color? backgroundColour;
+  final Color? textColour;
+
+  const StatBlock(
+      {super.key,
+      required this.statName,
+      required this.statValue,
+      this.backgroundColour,
+      this.textColour});
+  final String statName;
+  final int statValue;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      decoration: BoxDecoration(
+          color: backgroundColour ?? theme.colorScheme.primary,
+          borderRadius: BorderRadius.circular(16),
+          border: const BorderDirectional(bottom: BorderSide(width: 4))),
+      child: Column(
+        children: [
+          Text(
+            statName,
+            style: theme.textTheme.bodyMedium!.copyWith(
+              color: textColour ?? theme.colorScheme.onPrimary,
+            ),
+          ),
+          Text(
+            statValue.toString(),
+            style: theme.textTheme.titleLarge!.copyWith(
+              color: textColour ?? theme.colorScheme.onPrimary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StatsRow extends StatelessWidget {
+  const StatsRow({
+    super.key,
+    required this.xWins,
+    required this.draws,
+    required this.theme,
+    required this.oWins,
+  });
+
+  final int xWins;
+  final int draws;
+  final ThemeData theme;
+  final int oWins;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: StatBlock(
+              statName: "X WINS",
+              statValue: xWins,
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: StatBlock(
+              statName: "DRAwS",
+              statValue: draws,
+              backgroundColour: theme.colorScheme.surfaceContainer,
+              textColour: theme.colorScheme.onSurface,
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: StatBlock(
+              statName: "O WINS",
+              statValue: oWins,
+              backgroundColour: theme.colorScheme.secondary,
+              textColour: theme.colorScheme.onSecondary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/game.dart
+++ lib/game.dart
@@ -7,6 +7,7 @@ import 'package:tic_tac_toe/components/board.dart';
 import 'package:tic_tac_toe/components/custom_modal.dart';
 
 import 'package:tic_tac_toe/components/header.dart';
+import 'package:tic_tac_toe/components/stats.dart';
 
 enum GameMode {
   playerVsPlayer,
@@ -244,100 +245,6 @@ class _TicTacToeGameState extends State<TicTacToeGame> {
             if (showTerminalStateModal) _terminalStateModal(),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class StatsRow extends StatelessWidget {
-  const StatsRow({
-    super.key,
-    required this.xWins,
-    required this.draws,
-    required this.theme,
-    required this.oWins,
-  });
-
-  final int xWins;
-  final int draws;
-  final ThemeData theme;
-  final int oWins;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: StatBlock(
-              statName: "X WINS",
-              statValue: xWins,
-            ),
-          ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: StatBlock(
-              statName: "DRAwS",
-              statValue: draws,
-              backgroundColour: theme.colorScheme.surfaceContainer,
-              textColour: theme.colorScheme.onSurface,
-            ),
-          ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: StatBlock(
-              statName: "O WINS",
-              statValue: oWins,
-              backgroundColour: theme.colorScheme.secondary,
-              textColour: theme.colorScheme.onSecondary,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class StatBlock extends StatelessWidget {
-  final Color? backgroundColour;
-  final Color? textColour;
-
-  const StatBlock(
-      {super.key,
-      required this.statName,
-      required this.statValue,
-      this.backgroundColour,
-      this.textColour});
-  final String statName;
-  final int statValue;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      decoration: BoxDecoration(
-          color: backgroundColour ?? theme.colorScheme.primary,
-          borderRadius: BorderRadius.circular(16),
-          border: const BorderDirectional(bottom: BorderSide(width: 4))),
-      child: Column(
-        children: [
-          Text(
-            statName,
-            style: theme.textTheme.bodyMedium!.copyWith(
-              color: textColour ?? theme.colorScheme.onPrimary,
-            ),
-          ),
-          Text(
-            statValue.toString(),
-            style: theme.textTheme.titleLarge!.copyWith(
-              color: textColour ?? theme.colorScheme.onPrimary,
-            ),
-          ),
-        ],
       ),
     );
   }
